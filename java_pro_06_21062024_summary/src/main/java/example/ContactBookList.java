c62f4871ba621564da98c07c82c98e531ce40187
Natalia <kolchenko.nata.yu@gmail.com>
2024-06-26 19:52:48 +0200

    lesson_java_pro_21062024: ContactBook

--- java_pro_06_21062024_summary/src/main/java/example/ContactBookList.java
+++ java_pro_06_21062024_summary/src/main/java/example/ContactBookList.java
@@ -1,23 +1,5 @@
-package example;
-
-public class ContactBookList implements ContactBookInterface{
-    @Override
-    public Contact createContact() {
-        return null;
-    }
-
-    @Override
-    public Contact getContact() {
-        return null;
-    }
-
-    @Override
-    public Contact updateContact() {
-        return null;
-    }
-
-    @Override
-    public boolean deleteContact() {
-        return false;
-    }
-}
+//package example;
+//
+//public class ContactBookList implements ContactBookInterface{
+//
+//}
