40641fc2756c55b08de079e79c201e9b3c5953c1
Natalia <kolchenko.nata.yu@gmail.com>
2024-06-29 11:59:51 +0200

    lesson_java_pro_28062024: Lambda

---
+++ java_pro_06_28062024_summary/src/main/java/example/LambdaExample.java
@@ -0,0 +1,38 @@
+package example;
+
+import java.util.Arrays;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        String[] strArr = {"qwer", "", "qw", "e"};
+
+//        int[] res = where(strArr, item -> item.length(), Main::nameMethod);
+        int[] res = where(strArr, item -> item.length(), (strArr1, cl) -> nameMethod(strArr1, cl));
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] where(String[] data, CheckLength op, ArrayStringProcessor asp) {
+        return  asp.processArray(data, op);
+    }
+
+    static int[] nameMethod (String[] strArrItems, CheckLength cl) {
+        int[] numbArr = new int[strArrItems.length];
+        for (int i = 0; i < strArrItems.length; i++) {
+            numbArr[i] = cl.lengthOur(strArrItems[i]);
+
+        }
+        return numbArr;
+    }
+
+
+    @FunctionalInterface
+    interface CheckLength {
+        int lengthOur(String item);
+    }
+
+
+    @FunctionalInterface
+    interface ArrayStringProcessor {
+        int[] processArray (String[] strArr, CheckLength cl);
+    }
+}
